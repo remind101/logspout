472891d371645bcf1e54977387a885d4c3ae55b7
Benjamin Guillet <benjamin.guillet@gmail.com>
2015-09-15 00:25:30 -0700

    Notify honeybadger of errors.

--- honeybadger.go
+++ honeybadger.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	honeybadger "github.com/honeybadger-io/honeybadger-go"
+	"github.com/remind101/logspout-kinesis"
 )
 
 func init() {
@@ -11,4 +12,12 @@ func init() {
 		APIKey: os.Getenv("HONEYBADGER_API_KEY"),
 		Env:    os.Getenv("HONEYBADGER_ENVIRONMENT"),
 	})
+
+	kinesis.ErrorHandler = func(err error) {
+		if err != nil {
+			if _, ok := err.(*kinesis.StreamNotReadyError); !ok {
+				honeybadger.DefaultClient.Notify(err)
+			}
+		}
+	}
 }
