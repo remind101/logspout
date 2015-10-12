793e61390f1f43ddec662599e95c8da208a680f7
Benjamin Guillet <benjamin.guillet@gmail.com>
2015-10-12 11:17:22 -0700

    Verify honeybadger api key is present before activating honeybadger logging.

--- honeybadger.go
+++ honeybadger.go
@@ -8,15 +8,17 @@ import (
 )
 
 func init() {
-	honeybadger.Configure(honeybadger.Configuration{
-		APIKey: os.Getenv("HONEYBADGER_API_KEY"),
-		Env:    os.Getenv("HONEYBADGER_ENVIRONMENT"),
-	})
+	if apiKey := os.Getenv("HONEYBADGER_API_KEY"); apiKey != "" {
+		honeybadger.Configure(honeybadger.Configuration{
+			APIKey: apiKey,
+			Env:    os.Getenv("HONEYBADGER_ENVIRONMENT"),
+		})
 
-	kinesis.ErrorHandler = func(err error) {
-		if err != nil {
-			if _, ok := err.(*kinesis.StreamNotReadyError); !ok {
-				honeybadger.DefaultClient.Notify(err)
+		kinesis.ErrorHandler = func(err error) {
+			if err != nil {
+				if _, ok := err.(*kinesis.StreamNotReadyError); !ok {
+					honeybadger.DefaultClient.Notify(err)
+				}
 			}
 		}
 	}
