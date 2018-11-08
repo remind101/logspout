693663609913d46b676e0dc3234869af601fbd5a
Eric J. Holmes <eric@ejholmes.net>
2018-11-07 19:35:07 -0800

    Add an adapter for datadog logs

--- modules.go
+++ modules.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/gliderlabs/logspout/adapters/raw"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
 	_ "github.com/remind101/logspout-kinesis"
+	_ "github.com/remind101/logspout/adapters/datadog"
 	_ "github.com/remind101/logspout/adapters/dogstatsd"
 	_ "github.com/remind101/logspout/adapters/syslog"
 	_ "github.com/remind101/logspout/transports/udp"
